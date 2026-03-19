f4ab81c38af793c9e4d74172f54fc3f3f268be49
RohitKoliii <rohit.koli@junooncapital.in>
2026-03-20 04:04:31 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        // primary: "#003397",
+        // primary: "#ed502e",
+        primary: "#026fcf",
+        secondary: "#00e676",
+        success: "#00b809",
+        danger: "#ff0000",
+        warning: "#ffc107",
+        light: "#D3D3D3",
+        dark: "#848884",
+        white: "#fff",
+        black: "#000",
+      },
+    },
+  },
+  plugins: [],
+}
+
+// #5B99C2
+// #1F316F
